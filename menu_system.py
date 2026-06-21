4b5b626c5bee00a879b3096050099f260db3bb24
roqeeboduja <odujaroqeeb@gmail.com>
2026-06-21 23:52:46 +0100

    menu_system file added

--- menu_system.py
+++ menu_system.py
@@ -6,16 +6,19 @@ print(f"Hello! {name}")
 
 menu = ["1. Say Hello", "2. Tell a joke", "3. Exit"]
 
-for i in menu:
-    print(i)
+while True:
 
-choice = int(input("Enter your choice: "))
+    for i in menu:
+        print(i)
 
-if choice == 1:
-    print("Hello")
-elif choice == 2:
-    print("A fool will laugh when nothing is funny.")
-elif choice == 3:
-    print("Thank you.")
-else:
-    print("Invalid input")
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        print("Hello")
+    elif choice == 2:
+        print("A fool will laugh when nothing is funny.")
+    elif choice == 3:
+        print("Thank you.")
+        break
+    else:
+        print("Invalid input")
