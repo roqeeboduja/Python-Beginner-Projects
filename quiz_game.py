0fcb99d0fba43a6a71f76dc99c6a67afe1006c14
roqeeboduja <odujaroqeeb@gmail.com>
2026-06-22 23:31:05 +0100

    quiz_game.py file added

---
+++ quiz_game.py
@@ -0,0 +1,37 @@
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()      #quit() is a built=in function that quits a code.
+
+print("Okay!, Let's play 😊")
+
+score = 0
+
+answer = input("What does CPU stand for? ")
+
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+
+if answer.lower() == "graphic processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stand for? ").lower()     #another method for converting the string.
+
+if answer == "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+print(f"You got {score} question(s) correct!")
+print(f"You got {(score/3 )* 100}% correct!")
