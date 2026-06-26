bbe4ea1ef827a4c732ee9df7d8dd4061cd92f4df
roqeeboduja <odujaroqeeb@gmail.com>
2026-06-26 23:29:11 +0100

    Revert "comment all files"
    This reverts commit 1974f03be7608f5e5b0d9c405b5c603ed6ff8369.

--- menu_system.py
+++ menu_system.py
@@ -1,24 +1,24 @@
-# print("Hello!, Welcome to your menu system.")
+print("Hello!, Welcome to your menu system.")
 
-# name = input("Enter your name? ")
+name = input("Enter your name? ")
 
-# print(f"Hello! {name}")
+print(f"Hello! {name}")
 
-# menu = ["1. Say Hello", "2. Tell a joke", "3. Exit"]
+menu = ["1. Say Hello", "2. Tell a joke", "3. Exit"]
 
-# while True:
+while True:
 
-#     for i in menu:
-#         print(i)
+    for i in menu:
+        print(i)
 
-#     choice = int(input("Enter your choice: "))
+    choice = int(input("Enter your choice: "))
 
-#     if choice == 1:
-#         print("Hello")
-#     elif choice == 2:
-#         print("A fool will laugh when nothing is funny.")
-#     elif choice == 3:
-#         print("Thank you.")
-#         break
-#     else:
-#         print("Invalid input")
+    if choice == 1:
+        print("Hello")
+    elif choice == 2:
+        print("A fool will laugh when nothing is funny.")
+    elif choice == 3:
+        print("Thank you.")
+        break
+    else:
+        print("Invalid input")
--- number_guesser.py
+++ number_guesser.py
@@ -1,39 +1,41 @@
-# import random
+import random
 
-# range_limit = input("Type a number: ")
+range_limit = input("Type a number: ")
 
-# if range_limit.isdigit():
-#     range_limit = int(range_limit)  #typecasting the range_limit input 
+if range_limit.isdigit():
+    range_limit = int(range_limit)  #typecasting the range_limit input 
 
-#     if range_limit <= 0:
-#         print("Please type a number larger than zero.")
-#         quit()
+    if range_limit <= 0:
+        print("Please type a number larger than zero.")
+        quit()
 
-# else:
-#     print("Please type a number next time.")
-#     quit()
+else:
+    print("Please type a number next time.")
+    quit()
 
-# random_number = random.randrange(0, range_limit)        #one argument can be passed.
+random_number = random.randrange(0, range_limit)        #one argument can be passed.
 
-# guesses = 0
+guesses = 0
 
-# while True:
-#     guesses+=1
-#     user_guess = input("Make a guess: ")
-#     if user_guess.isdigit():
-#         user_guess = int(user_guess)  #typecasting the range_limit input 
-#     else:
-#         print("Please type a number next time.")
-#         continue
+while True:
+    guesses+=1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)  #typecasting the range_limit input 
+    else:
+        print("Please type a number next time.")
+        continue
 
-#     if user_guess == random_number:
-#         print("You got it!")
-#         break
-#     elif user_guess > random_number:
-#         print("You're above the number.")
-#     else:
-#         print("You're below the number.")
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    elif user_guess > random_number:
+        print("You're above the number.")
+    else:
+        print("You're below the number.")
 
-# print(f"You got it in {guesses} guess(es)")
+print(f"You got it in {guesses} guess(es)")
     
-# # random.randint(-5, 11)
+# random.randint(-5, 11)
+
+
